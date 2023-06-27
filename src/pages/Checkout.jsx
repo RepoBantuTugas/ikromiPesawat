369eb3add65f6f252263d3786ed3fb95ec91b389
Latifano <ikromilatifano76@gmail.com>
2023-06-27 20:36:00 +0700

    add checkout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import Datapemesanan from "../components/Datapemesanan";
-import Datapenumpang from "../components/DataPenumpang";
+import Datapenumpang from "../components/Datapenumpang";
 // import Matahari from "../styles/images/matahari.png";
 import "../styles/checkout.css";
 import {
@@ -15,7 +15,7 @@ import {
 
 const Checkout = () => {
   const loc = useLocation();
-  const data = loc.state;
+  const dataPost = loc.state;
   // useEffect(() => {
   //   for (data.price.adult_count; data.price.adult_count > 0; data.price.adult_count--) {
   //     console.log("komponen data looping ke ",data.price.adult_count)
@@ -69,7 +69,7 @@ const Checkout = () => {
               <Datapemesanan />
             </Row>
             <Row className="border py-4 px-4 mt-3">
-            <Datapenumpang />
+              <Datapenumpang />
             </Row>
             <Button type="submit" className="w-100 my-3 button-save">
               Simpan
@@ -79,13 +79,13 @@ const Checkout = () => {
             <div className="mt-3">
               <h5 className="fw-bold">Detail Penerbangan</h5>
               <div className="d-flex justify-content-between align-items-center">
-                <h5 className="fw-bold">07:00</h5>
+                <h5 className="fw-bold">{dataPost.departure.departure_time}</h5>
                 <h6 className="fw-bold" style={{ color: "#7126B5" }}>
                   Keberangkatan
                 </h6>
               </div>
-              <p className="mb-0">3 Maret 2023</p>
-              <p>Soekarno Hatta - Terminal 1A Domestik</p>
+              <p className="mb-0">{dataPost.departure.date}</p>
+              <p>{dataPost.departure.airport_name}</p>
             </div>
 
             <hr />
@@ -96,11 +96,15 @@ const Checkout = () => {
                 {/* <img src={Matahari} alt="" /> */}
               </Col>
               <Col md="auto">
-                <h6 className="fw-bold">Jet Air - Economy</h6>
-                <h6 className="fw-bold mb-4">JT - 203</h6>
+                <h6 className="fw-bold">
+                  {dataPost.info.airline_name} - {dataPost.info.seat_class}
+                </h6>
+                <h6 className="fw-bold mb-4">{dataPost.info.airplane_model}</h6>
                 <h6 className="fw-bold">Informasi:</h6>
-                <p className="mb-0">Baggage 20kg</p>
-                <p className="mb-0">Cabin baggage 7 kg</p>
+                <p className="mb-0">Baggage {dataPost.info.free_baggage} kg</p>
+                <p className="mb-0">
+                  Cabin baggage {dataPost.info.cabin_baggage} kg
+                </p>
                 <p>In Flight Entertainment</p>
               </Col>
             </Row>
@@ -109,13 +113,13 @@ const Checkout = () => {
 
             <div>
               <div className="d-flex justify-content-between align-items-center">
-                <h5 className="fw-bold">11:00</h5>
+                <h5 className="fw-bold">{dataPost.arrival.arrival_time}</h5>
                 <h6 className="fw-bold" style={{ color: "#7126B5" }}>
                   Kedatangan
                 </h6>
               </div>
-              <p className="mb-0">3 Maret 2023</p>
-              <p className="fw-bold">Melbourne International Airport</p>
+              <p className="mb-0">{dataPost.arrival.date}</p>
+              <p className="fw-bold">{dataPost.arrival.airport_name}</p>
             </div>
 
             <hr />
@@ -123,16 +127,20 @@ const Checkout = () => {
             <div>
               <h5 className="fw-bold">Rincian Harga</h5>
               <div className="d-flex justify-content-between align-items-center">
-                <p>2 Adults</p>
-                <p>IDR 9.550.000</p>
+                <p>{dataPost.price.adult_count} Adults</p>
+                <p>IDR {dataPost.price.adult_price}</p>
               </div>
               <div className="d-flex justify-content-between align-items-center">
-                <p>1 Baby</p>
-                <p>IDR 0</p>
+                <p>{dataPost.price.child_count} Baby</p>
+                <p>IDR {dataPost.price.child_price}</p>
+              </div>
+              <div className="d-flex justify-content-between align-items-center">
+                <p>{dataPost.price.infant_count} Baby</p>
+                <p>IDR {dataPost.price.infant_price}</p>
               </div>
               <div className="d-flex justify-content-between align-items-center">
                 <p>Tax</p>
-                <p>IDR 300.000</p>
+                <p>IDR {dataPost.price.tax}</p>
               </div>
             </div>
 
@@ -141,7 +149,7 @@ const Checkout = () => {
             <div className="d-flex justify-content-between align-items-center">
               <h5 className="fw-bold txt-primary">Total</h5>
               <h5 className="fw-bold" style={{ color: "#7126B5" }}>
-                IDR 9.850.000
+                IDR {dataPost.price.total_price}
               </h5>
             </div>
             <br />
